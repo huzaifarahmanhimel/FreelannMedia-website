067256e2fca2295162bb91ee62fd1969bbb8e60f
Md. Huzaifa Rahman Himel <himel6688@gmail.com>
2025-09-08 04:06:37 +0600

    update

--- Agency/src/App.jsx
+++ Agency/src/App.jsx
@@ -2,14 +2,6 @@
 import './App.css';
 
 import { createBrowserRouter, Route, BrowserRouter as Router, RouterProvider, Routes } from 'react-router-dom';
-
-import Contactus from './components/contactuspage/Contactus';
-import DancingPages from './components/dancing/Dancingpages';
-import FoodBusiness from './components/foodpage/Foodbusiness';
-import HomePage from './components/home/HomePage';
-import MassagwepageFull from './components/massagepage/MassagepageFull';
-import TennisPage from './components/tennis/TennisPage';
-import AppLayout from './components/ui/AppLayout';
 import Hero from './Components/hero';
 
 
@@ -20,17 +12,9 @@ const router = createBrowserRouter([
     element: <AppLayout />,
     children: [
       { path: '/', element: <Hero /> },
-      { path: '/', element: <HomePage /> },
-      { path: "/contactus", element: <Contactus /> },
-      { path: "/MassageParlour", element: <MassagwepageFull /> },
-      { path: "/FoodBusiness", element: <FoodBusiness /> },
-      { path: "/DancingPages", element: <DancingPages /> },
-      { path: "/TennisPage", element: <TennisPage /> }
     ]
   }]
 );
-
-
 
 
 function App() {
